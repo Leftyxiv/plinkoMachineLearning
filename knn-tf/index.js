bd82023b2e4e6b216961cdb6e9db70c23637a670
Leftyxiv <manny@aledoux.net>
2021-05-24 09:54:53 -0400

    added logic to load csv data

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -1,4 +1,11 @@
 require('@tensorflow/tfjs-node')
 const tf = require('@tensorflow/tfs')
 
-const
+const loadCSV = require('./load-csv')
+
+let { features, labels, testFeatures, testLabels } = loadCSV('kc_house_data.csv', {
+  shuffle: true,
+  splitTest: 10,
+  dataColumns: ['lat', 'long'],
+  labelColumns: ['price']
+});

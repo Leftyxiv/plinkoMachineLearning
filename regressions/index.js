c64fff309182d0fe7cbd1d65a24f07994498a74d
Leftyxiv <manny@aledoux.net>
2021-05-30 17:43:36 -0400

    changed gradient descent method to use tf methods and multimatrix multiplication

--- regressions/index.js
+++ regressions/index.js
@@ -1,11 +1,21 @@
-require('@tensorflow/tfjs-node');
-const tf = require('@tensorflow/tfjs');
-const loadCSV = require('./load-csv');
+require("@tensorflow/tfjs-node");
+const tf = require("@tensorflow/tfjs");
+const loadCSV = require("./load-csv");
 
-let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
+const LinerRegression = require("./linearRegression");
+
+let { features, labels, testFeatures, testLabels } = loadCSV("./cars.csv", {
   shuffleL: true,
   splitTest: 50,
-  dataColumns: ['horsepower'],
-  labelColumns: ['mpg'],
-})
+  dataColumns: ["horsepower"],
+  labelColumns: ["mpg"],
+});
 
+const regression = new LinerRegression(features, labels, {
+  learningRate: 0.0001,
+  iterations: 100,
+});
+
+regression.train();
+
+console.log(regression.weights.get(1, 0), "-----m-----", regression.weights.get(0, 0), "-----b-------");

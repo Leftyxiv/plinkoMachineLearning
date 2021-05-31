9b120a3022a3c1574790a2376aee60ba19f8110b
Leftyxiv <manny@aledoux.net>
2021-05-31 09:09:17 -0400

    added plot

--- regressions/index.js
+++ regressions/index.js
@@ -1,6 +1,7 @@
 require("@tensorflow/tfjs-node");
 const tf = require("@tensorflow/tfjs");
 const loadCSV = require("./load-csv");
+const plot = require('node-remote-plot');
 
 const LinerRegression = require("./linearRegression");
 
@@ -12,10 +13,16 @@ let { features, labels, testFeatures, testLabels } = loadCSV("./cars.csv", {
 });
 
 const regression = new LinerRegression(features, labels, {
-  learningRate: 0.1,
+  learningRate: 10,
   iterations: 100,
 });
 
 regression.train();
 const r2 = regression.test(testFeatures, testLabels);
+
+plot({
+  x: regression.mseHistory.reverse(),
+  xLabel: 'Iteration #',
+  yLabel: 'Mean Squared Error'
+})
 console.log('r2', r2)
--- regressions/linearRegression.js
+++ regressions/linearRegression.js
@@ -43,13 +43,13 @@ class LinerRegression {
 
   processFeatures(features) {
     features = tf.tensor(features);
-    
-    if(this.mean && this.variance){
-      features = features.sub(this.mean).div(this.variance.pow(.5));
+
+    if (this.mean && this.variance) {
+      features = features.sub(this.mean).div(this.variance.pow(0.5));
     } else {
       features = this.standardize(features);
     }
-    
+
     features = tf.ones([features.shape[0], 1]).concat(features, 1);
 
     return features;
@@ -57,28 +57,27 @@ class LinerRegression {
 
   standardize(features) {
     const { mean, variance } = tf.moments(features, 0);
-    this.mean = mean
-    this.variance = variance
+    this.mean = mean;
+    this.variance = variance;
 
-    return features.sub(mean).div(variance.pow(.5));
+    return features.sub(mean).div(variance.pow(0.5));
   }
 
-  recordMSE(){
+  recordMSE() {
     const mse = this.features.matMul(this.weights).sub(this.labels).pow(2).sum().div(this.features.shape[0]).get();
     this.mseHistory.unshift(mse);
   }
 
   updateLearningRate() {
-    if (this.mseHistory.length < 2){
+    if (this.mseHistory.length < 2) {
       return;
     }
-    if (this.mseHistory[0] > this.mseHistory[1]){
+    if (this.mseHistory[0] > this.mseHistory[1]) {
       this.options.learningRate /= 2;
     } else {
       this.options.learningRate *= 1.05;
     }
   }
-
 }
 
 module.exports = LinerRegression;

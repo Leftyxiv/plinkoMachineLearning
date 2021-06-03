7d39c5672e2d444f465e21d61622b47e89272db2
Leftyxiv <manny@aledoux.net>
2021-06-03 16:26:13 -0400

    added memory optimization

--- regressions/multinominal-logistic-regression/index.js
+++ regressions/multinominal-logistic-regression/index.js
@@ -5,24 +5,30 @@ const mnist = require('mnist-data');
 const LogisticRegression = require('./logisticRegression');
 const plot = require('node-remote-plot');
 
-const mnistData = mnist.training(0, 1000)
+function loadData(){
 
-const features = mnistData.images.values.map(image => _.flatMap(image))
-const encodedLabels = mnistData.labels.values.map(label => {
-  const row = new Array(10).fill(0)
-  row[label] = 1
-  return row;
-});
+  const mnistData = mnist.training(0, 5000)
+  
+  const features = mnistData.images.values.map(image => _.flatMap(image))
+  const encodedLabels = mnistData.labels.values.map(label => {
+    const row = new Array(10).fill(0)
+    row[label] = 1
+    return row;
+  });
+  return {features, labels: encodedLabels}
+}
 
-const regression = new LogisticRegression(features, encodedLabels, {
+const { features, labels } = loadData();
+  
+const regression = new LogisticRegression(features, labels, {
   learningRate: 1,
-  iterations: 5,
+  iterations: 20,
   batchSize: 100
 })
 
 regression.train();
 
-const testMnistData = mnist.testing(0, 100);
+const testMnistData = mnist.testing(0, 1000);
 const testFeatures = testMnistData.images.values.map(image => _.flatMap(image));
 const testEncodedLabels = testMnistData.labels.values.map(label => {
   const row = new Array(10).fill(0)
